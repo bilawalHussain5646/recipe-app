130eaab85d65c6f961ed2d611f60755d9a1cc815
Bilawal Hussain <bilawal.hussain350@gmail.com>
2025-10-01 11:15:15 +0400

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// If deploying to GitHub Pages under: https://<username>.github.io/<repo-name>/
-// set base to '/<repo-name>/'. Otherwise set base to '/'.
 export default defineConfig({
-  base: "/recipe-app/", // <--- CHANGE THIS for GitHub Pages (or '/' if not)
   plugins: [vue()],
+  base: "/recipe-app/", // <-- important for GitHub Pages
 });
